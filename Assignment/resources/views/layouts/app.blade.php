ccbb3ed4b8668ff4f0618c66bce4595621b84276
u1357447 <U1357447@unimail.hud.ac.uk>
2017-01-11 17:54:00 +0000

    Added Header Image with a custom css style

--- Assignment/resources/views/layouts/app.blade.php
+++ Assignment/resources/views/layouts/app.blade.php
@@ -12,6 +12,7 @@
 
     <!-- Styles -->
     <link href="/css/app.css" rel="stylesheet">
+    <link href="/css/custom.css" rel="stylesheet">
 
     <!-- Scripts -->
     <script src="/js/app.js"></script>
@@ -70,6 +71,8 @@
                 </div>
             </div>
         </nav>
+
+        <div class="header"></div>
 
         <div class="container">
             <div class="row">
